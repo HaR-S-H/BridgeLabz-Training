6f250a7e71b01375840891ceff34db5c5ab5d514
HaR-S-H <h8551283@gmail.com>
2025-12-18 14:32:36 +0530

    [Harsh Gupta] add KilometersToMiles

---
+++ core-csharp-practice/gcr-codebase/programming-elements/KilometersToMiles.cs
@@ -0,0 +1,10 @@
+using System;
+class KilometersToMiles
+{
+    static void Main()
+    {
+        double kilometers = 10.0;
+        double miles = kilometers * 0.621371;
+        Console.Write(miles);
+    }
+}
