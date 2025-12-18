de7c72ff69b913587ca2d37381d543c0c14471ca
HaR-S-H <h8551283@gmail.com>
2025-12-18 14:46:22 +0530

    [Harsh Gupta] add PowerCalculation

---
+++ core-csharp-practice/gcr-codebase/programming-elements/PowerCalculation.cs
@@ -0,0 +1,11 @@
+using System;
+class PowerCalculation
+{
+    static void Main()
+    {
+        double baseNum = 2.0;
+        double exponent = 3.0;
+        double result = Math.Pow(baseNum, exponent);
+        Console.Write(result);
+    }
+}
