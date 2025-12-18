37dbca8cdbe67cafc2644fe70a65d204fecada70
HaR-S-H <h8551283@gmail.com>
2025-12-18 14:38:04 +0530

    [Harsh Gupta] add AverageOfThreeNumbers

---
+++ core-csharp-practice/gcr-codebase/programming-elements/AverageOfThreeNumbers.cs
@@ -0,0 +1,12 @@
+using System;
+class AverageOfThreeNumbers
+{
+    static void Main()
+    {
+        double num1 = 10.0;
+        double num2 = 20.0;
+        double num3 = 30.0;
+        double average = (num1 + num2 + num3) / 3;
+        Console.Write(average);
+    }
+}
