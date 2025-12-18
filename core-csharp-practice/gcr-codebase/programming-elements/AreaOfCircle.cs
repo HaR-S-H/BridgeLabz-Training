520147996d3bc8394ea3d6144ff9a323e81691f0
HaR-S-H <h8551283@gmail.com>
2025-12-18 14:28:56 +0530

    [Harsh Gupta] add AreaOfCircle

---
+++ core-csharp-practice/gcr-codebase/programming-elements/AreaOfCircle.cs
@@ -0,0 +1,10 @@
+using System;
+class AreaOfCircle
+{
+    static void Main()
+    {
+        double radius = 7.5;
+        double area = Math.PI * radius * radius;
+        Console.Write(area);
+    }
+}
