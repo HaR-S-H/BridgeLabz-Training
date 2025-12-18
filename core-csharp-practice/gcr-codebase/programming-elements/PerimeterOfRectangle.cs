691e15b6556703114312a4dd407a6873022dff66
HaR-S-H <h8551283@gmail.com>
2025-12-18 14:48:57 +0530

    [Harsh Gupta] add PerimeterOfRectangle

---
+++ core-csharp-practice/gcr-codebase/programming-elements/PerimeterOfRectangle.cs
@@ -0,0 +1,11 @@
+using System;
+class PerimeterOfRectangle
+{
+    static void Main()
+    {
+        double length = 5.0;
+        double width = 3.0;
+        double perimeter = 2 * (length + width);
+        Console.Write(perimeter);
+    }
+}
