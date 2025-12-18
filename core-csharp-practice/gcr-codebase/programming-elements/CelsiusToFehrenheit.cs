95c1a18ef32881ec0d6cbef3a5758b7ace3637a0
HaR-S-H <h8551283@gmail.com>
2025-12-18 14:34:48 +0530

    [Harsh Gupta add CelsiusToFehrenheit]

---
+++ core-csharp-practice/gcr-codebase/programming-elements/CelsiusToFehrenheit.cs
@@ -0,0 +1,10 @@
+using System;
+class CelsiusToFehrenheit
+{
+    static void Main()
+    {
+        double celsius = 25.0;
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        Console.Write(fahrenheit);
+    }
+}
