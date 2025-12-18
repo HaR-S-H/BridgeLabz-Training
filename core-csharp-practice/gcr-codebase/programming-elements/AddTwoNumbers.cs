7c6eb7e9bbdf9b5a8a855b1b3a5852ac448baf01
HaR-S-H <h8551283@gmail.com>
2025-12-18 12:35:17 +0530

    [Harsh Gupta] AddTwoNumbers

---
+++ core-csharp-practice/gcr-codebase/programming-elements/AddTwoNumbers.cs
@@ -0,0 +1,11 @@
+using System;
+class AddTwoNumbers
+{
+    static void Main(string[] args)
+    {
+        int n1 = 1;
+        int n2 = 2;
+        int sum = n1 + n2;
+        Console.WriteLine(sum);
+    }
+}
