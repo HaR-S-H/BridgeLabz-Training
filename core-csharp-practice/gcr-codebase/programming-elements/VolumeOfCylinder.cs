ffb416eea4ea39572acbaafb353c6e1db495c735
HaR-S-H <h8551283@gmail.com>
2025-12-18 14:10:33 +0530

    [Harsh Gupta] add VolumeOfCylinder

---
+++ core-csharp-practice/gcr-codebase/programming-elements/VolumeOfCylinder.cs
@@ -0,0 +1,11 @@
+using System;
+class VolumeOfCylinder
+{
+    static void Main()
+    {
+        double r = 5.0;
+        double h = 10.0;
+        double volume = Math.PI * r * r * h;
+        Console.WriteLine(volume);
+    }
+}
