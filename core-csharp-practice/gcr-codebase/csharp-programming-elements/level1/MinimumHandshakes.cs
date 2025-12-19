6cebf73694458018981fad1d32b3730df6b38166
HaR-S-H <h8551283@gmail.com>
2025-12-19 15:50:00 +0530

    [Harsh Gupta] add MinimumHandshakes

---
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level1/MinimumHandshakes.cs
@@ -0,0 +1,14 @@
+using System;
+
+class MinimumHandshakes
+{
+    static void Main()
+    {
+        Console.Write("Enter number of students: ");
+        int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+
+        Console.WriteLine($"The maximum number of handshakes is {handshakes}");
+    }
+}
