0c799cfe03e43679b15dbd4da0a4c258e8ad43ee
HaR-S-H <h8551283@gmail.com>
2025-12-19 15:33:08 +0530

    [Harsh Gupta] add KilometersToMilesUserInput

---
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level1/kilometersToMiles(UserInput).cs
@@ -0,0 +1,14 @@
+using System;
+
+class KilometersToMilesUserInput
+{
+    static void Main()
+    {
+        Console.Write("Enter distance in km: ");
+        double km = Convert.ToDouble(Console.ReadLine());
+
+        double miles = km / 1.6;
+
+        Console.WriteLine($"The total miles is {miles} mile for the given {km} km");
+    }
+}
