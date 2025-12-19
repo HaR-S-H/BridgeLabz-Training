070291db77a7c9a3391d380749efb540af4529b6
HaR-S-H <h8551283@gmail.com>
2025-12-19 17:00:43 +0530

    [Harsh Gupta] add Fehrenheit To Celsius

---
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level2/FehrenheitToCelsius.cs
@@ -0,0 +1,14 @@
+using System;
+
+class FehrenheitToCelsius
+{
+    static void Main()
+    {
+        Console.Write("Enter temperature in Fahrenheit: ");
+        double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        Console.WriteLine($"The {fahrenheit} Fahrenheit is {celsius} Celsius");
+    }
+}
