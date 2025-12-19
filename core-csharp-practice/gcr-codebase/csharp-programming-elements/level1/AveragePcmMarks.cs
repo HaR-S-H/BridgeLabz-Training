3824d41323bd2bbafbdaaaef7467dc0096e291e7
HaR-S-H <h8551283@gmail.com>
2025-12-19 15:18:16 +0530

    [Harsh Gupta] add KilometersToMiles

--- core-csharp-practice/gcr-codebase/csharp-programming-elements/level1/AveragePcmMarks.cs
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level1/AveragePcmMarks.cs
@@ -1,6 +1,6 @@
 using System;
 
-class Program
+class AveragePcmMarks
 {
     static void Main()
     {
---
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level1/KilometersToMiles.cs
@@ -0,0 +1,12 @@
+using System;
+
+class KilometersToMiles
+{
+    static void Main()
+    {
+        double km = 10.8;
+        double miles = km / 1.6;
+
+        Console.WriteLine($"The distance {km} km in miles is {miles}");
+    }
+}
