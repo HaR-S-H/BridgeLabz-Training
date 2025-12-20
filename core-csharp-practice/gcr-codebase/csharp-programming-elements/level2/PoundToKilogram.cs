be228e61b4786c7259042a0836cd2317edd225aa
HaR-S-H <h8551283@gmail.com>
2025-12-20 14:42:43 +0530

    [Harsh Gupta] add PoundToDistribution

---
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level2/PoundToKilogram.cs
@@ -0,0 +1,14 @@
+using System;
+
+class PoundToKilogram
+{
+    static void Main()
+    {
+        Console.Write("Enter weight in pounds: ");
+        double pounds = Convert.ToDouble(Console.ReadLine());
+
+        double kg = pounds / 2.2;
+
+        Console.WriteLine($"The weight of the person in pounds is {pounds} and in kg is {kg}");
+    }
+}
