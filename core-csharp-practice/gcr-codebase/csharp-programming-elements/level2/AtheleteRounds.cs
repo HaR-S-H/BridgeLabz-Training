8f1ab38922be1cbcd9aec1e8ce8691b98ca109f5
HaR-S-H <h8551283@gmail.com>
2025-12-20 14:38:13 +0530

    [Harsh Gupta] add AtheleteRounds

---
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level2/AtheleteRounds.cs
@@ -0,0 +1,21 @@
+using System;
+
+class AtheleteRounds
+{
+    static void Main()
+    {
+        Console.Write("Enter side1 in meters: ");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter side2 in meters: ");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter side3 in meters: ");
+        double side3 = Convert.ToDouble(Console.ReadLine());
+
+        double perimeter = side1 + side2 + side3;
+        double rounds = 5000 / perimeter;
+
+        Console.WriteLine($"The total number of rounds the athlete will run is {rounds} to complete 5 km");
+    }
+}
