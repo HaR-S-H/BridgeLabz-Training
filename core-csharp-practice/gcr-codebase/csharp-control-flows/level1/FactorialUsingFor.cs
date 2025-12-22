40735cefc673c700acfb8d2f4b56765a65c04de9
HaR-S-H <h8551283@gmail.com>
2025-12-22 15:31:42 +0530

    [Harsh Gupta] add FactorialUsingFor

---
+++ core-csharp-practice/gcr-codebase/csharp-control-flows/level1/FactorialUsingFor.cs
@@ -0,0 +1,25 @@
+using System;
+
+class FactorialUsingFor
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < 0)
+        {
+            Console.WriteLine("Factorial is not defined for negative numbers.");
+            return;
+        }
+
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"The factorial of {number} is {factorial}");
+    }
+}
