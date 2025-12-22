a319b768cc218ddeb46f5b25b2a59d3200154f51
HaR-S-H <h8551283@gmail.com>
2025-12-22 16:38:33 +0530

    [Harsh Gupta] add LeapYear

---
+++ core-csharp-practice/gcr-codebase/csharp-control-flows/level2/leapYear.cs
@@ -0,0 +1,38 @@
+using System;
+class LeapYear
+{
+    static void Main()
+    {
+        Console.Write("Enter a year: ");
+        int yr = int.Parse(Console.ReadLine());
+        if (yr >= 1582)
+        {
+            if (yr % 4 == 0)
+            {
+                if (yr % 100 == 0)
+                {
+                    if (yr % 400 == 0)
+                    {
+                        Console.WriteLine(yr + " is a Leap Year");
+                    }
+                    else
+                    {
+                        Console.WriteLine(yr + " is not a Leap Year");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(yr + " is a Leap Year");
+                }
+            }
+            else
+            {
+                Console.WriteLine(yr + " is not a Leap Year");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Year must be >= 1582");
+        }
+    }
+}
