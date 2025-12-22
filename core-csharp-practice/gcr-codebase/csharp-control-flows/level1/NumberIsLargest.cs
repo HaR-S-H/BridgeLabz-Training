fafd8579b0aca538f723008819d908213ca00b31
HaR-S-H <h8551283@gmail.com>
2025-12-22 14:28:03 +0530

    [Harsh Gupta] add NumberIsLargest

---
+++ core-csharp-practice/gcr-codebase/csharp-control-flows/level1/NumberIsLargest.cs
@@ -0,0 +1,18 @@
+using System;
+
+class NumberIsLargest
+{
+    static void Main()
+    {
+        Console.Write("Enter number1: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Enter number2: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.Write("Enter number3: ");
+        int c = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Is the first number the largest? {a > b && a > c}");
+        Console.WriteLine($"Is the second number the largest? {b > a && b > c}");
+        Console.WriteLine($"Is the third number the largest? {c > a && c > b}");
+    }
+}
