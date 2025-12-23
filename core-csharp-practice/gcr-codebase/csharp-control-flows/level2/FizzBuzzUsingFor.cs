5292d5eaa0f026ae9f7a4821e1f2e4a1bea0fc01
HaR-S-H <h8551283@gmail.com>
2025-12-23 11:53:12 +0530

    [Harsh Gupta] add FizzBuzzUsingFor

---
+++ core-csharp-practice/gcr-codebase/csharp-control-flows/level2/FizzBuzzUsingFor.cs
@@ -0,0 +1,21 @@
+using System;
+class FizzBuzzUsingFor
+{
+    static void Main()
+    {
+        Console.Write("Enter a positive number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= num; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                Console.WriteLine("FizzBuzz");
+            else if (i % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if (i % 5 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(i);
+        }
+    }
+}
