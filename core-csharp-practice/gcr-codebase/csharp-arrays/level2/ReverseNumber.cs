d70c600a3e9bfd00459d573d917901f4be7656f8
HaR-S-H <h8551283@gmail.com>
2025-12-23 15:54:16 +0530

    [Harsh Gupta] add ReverseNumber

---
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level2/ReverseNumber.cs
@@ -0,0 +1,34 @@
+using System;
+
+class ReverseNumber
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        int temp = number;
+        int count = 0;
+
+        while (temp != 0)
+        {
+            count++;
+            temp = temp / 10;
+        }
+
+        int[] digits = new int[count];
+        temp = number;
+
+        for (int i = 0; i < count; i++)
+        {
+            digits[i] = temp % 10;
+            temp = temp / 10;
+        }
+
+        Console.WriteLine("Reversed Number:");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(digits[i]);
+        }
+    }
+}
