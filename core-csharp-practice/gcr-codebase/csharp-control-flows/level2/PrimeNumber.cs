6ba72b745a904c1b7135102e2b8cdb20880b47d9
HaR-S-H <h8551283@gmail.com>
2025-12-23 11:52:14 +0530

    [Harsh Gupta] add PrimeNumber

---
+++ core-csharp-practice/gcr-codebase/csharp-control-flows/level2/PrimeNumber.cs
@@ -0,0 +1,35 @@
+using System;
+class PrimeNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+        bool isPrime = true;
+
+        if (n <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine(n + " is Prime");
+        }
+        else
+        {
+            Console.WriteLine(n + " is not Prime");
+        }
+    }
+}
