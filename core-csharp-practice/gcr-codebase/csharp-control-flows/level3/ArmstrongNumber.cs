204316510d771bc4dbb0716b30fb82e0732b215b
HaR-S-H <h8551283@gmail.com>
2025-12-23 14:38:33 +0530

    [Harsh Gupta] add ArmstrongNumber

---
+++ core-csharp-practice/gcr-codebase/csharp-control-flows/level3/ArmstrongNumber.cs
@@ -0,0 +1,33 @@
+using System;
+
+class ArmstrongNumber
+{
+    static void Main()
+    {
+        int number;
+        int originalNumber;
+        int remainder;
+        int sum = 0;
+
+        Console.WriteLine("Enter a number:");
+        number = int.Parse(Console.ReadLine());
+
+        originalNumber = number;
+
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            sum = sum + (remainder * remainder * remainder);
+            originalNumber = originalNumber / 10;
+        }
+
+        if (sum == number)
+        {
+            Console.WriteLine("It is an Armstrong Number");
+        }
+        else
+        {
+            Console.WriteLine("It is NOT an Armstrong Number");
+        }
+    }
+}
