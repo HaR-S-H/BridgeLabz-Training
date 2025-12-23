0c63dfc50b561f16a37b2f824f9c22339ce1c459
HaR-S-H <h8551283@gmail.com>
2025-12-23 15:02:00 +0530

    [Harsh Gupta] add OddEvenArray

---
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level1/OddEvenArray.cs
@@ -0,0 +1,48 @@
+using System;
+
+class OddEvenArray
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number <= 0)
+        {
+            Console.WriteLine("Invalid input. Not a natural number.");
+            return;
+        }
+
+        int[] even = new int[number / 2 + 1];
+        int[] odd = new int[number / 2 + 1];
+
+        int evenIndex = 0;
+        int oddIndex = 0;
+
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 2 == 0)
+            {
+                even[evenIndex] = i;
+                evenIndex++;
+            }
+            else
+            {
+                odd[oddIndex] = i;
+                oddIndex++;
+            }
+        }
+
+        Console.WriteLine("Even Numbers:");
+        for (int i = 0; i < evenIndex; i++)
+        {
+            Console.WriteLine(even[i]);
+        }
+
+        Console.WriteLine("Odd Numbers:");
+        for (int i = 0; i < oddIndex; i++)
+        {
+            Console.WriteLine(odd[i]);
+        }
+    }
+}
