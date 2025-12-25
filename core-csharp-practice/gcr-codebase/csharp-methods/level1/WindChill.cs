d49b76e3a881e1ba8f307165bcbbee0175cc5e35
HaR-S-H <h8551283@gmail.com>
2025-12-25 18:05:20 +0530

    [Harsh Gupta] add WindChill

---
+++ core-csharp-practice/gcr-codebase/csharp-methods/level1/WindChill.cs
@@ -0,0 +1,21 @@
+using System;
+
+class WindChill
+{
+    public static double CalculateWindChill(double temp, double windSpeed)
+    {
+        return 35.74 + 0.6215 * temp +
+               (0.4275 * temp - 35.75) * Math.Pow(windSpeed, 0.16);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter temperature: ");
+        double t = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter wind speed: ");
+        double w = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Wind Chill = " + CalculateWindChill(t, w));
+    }
+}
