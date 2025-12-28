69295530a77d9c3ac79d48c4353e914a66d3bdcd
HaR-S-H <h8551283@gmail.com>
2025-12-28 21:36:58 +0530

    [Harsh Gupta] add RemoveDuplicates

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/RemoveDuplicates.cs
@@ -0,0 +1,25 @@
+using System;
+
+class RemoveDuplicates
+{
+    static string RemoveDuplicates(string text)
+    {
+        string result = "";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (!result.Contains(text[i]))
+                result += text[i];
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter string: ");
+        string s = Console.ReadLine();
+
+        Console.WriteLine("After removing duplicates: " + RemoveDuplicates(s));
+    }
+}
