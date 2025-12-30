68d9d0a9e437fcaa99a286896b6d0676b944043e
HaR-S-H <h8551283@gmail.com>
2025-12-30 16:11:39 +0530

    [Harsh Gupta] add ProductInventory

--- oops-csharp-practice/gcr-codebase/csharp-constructors/level1/CarRental.cs
+++ oops-csharp-practice/gcr-codebase/csharp-constructors/level1/CarRental.cs
@@ -1,26 +1,27 @@
 using System;
 class CarRental
-{
+{//instance variable
     private string customerName;
     private string carModel;
     private int retalDays;
-
+    //parameterized constructor
     public CarRental(string customerName, string carModel, int retalDays)
     {
         this.customerName = customerName;
         this.carModel = carModel;
         this.retalDays = retalDays;
     }
+    //method for calculating cost
     public void CalculateCost()
     {
         Console.Write("Total Cost :- " + 100 * retalDays);
     }
 }
 class Program
-{
+{//entry point
     static void Main()
-    {
-        CarRental car = new CarRental("harsh", "xuv700", 2);
+    {   //object creation
+        CarRental car = new CarRental("harsh", "xuv700", 2); //calling p
         car.CalculateCost();
     }
 }
