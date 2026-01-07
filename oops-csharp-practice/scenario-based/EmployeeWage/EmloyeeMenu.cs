eaea0546dab68d1506a8cdf66711bacb6d58df2d
HaR-S-H <h8551283@gmail.com>
2026-01-07 12:32:11 +0530

    [Harsh Gupta] add EmployeeWage and done uc version 1

---
+++ oops-csharp-practice/scenario-based/EmployeeWage/EmloyeeMenu.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWage
+{
+    sealed class EmployeeMenu
+    {
+
+        private readonly IEmployee employeeService;
+
+        public EmployeeMenu()
+        {
+            employeeService = new EmployeeUtilityImpl();
+        }
+        public void Choices()
+        {
+            int choice;
+
+            do
+            {
+                Console.WriteLine("\n==== Employee Wage Computation ====");
+                Console.WriteLine("1. UC1 - Check Attendance");
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter Choice: ");
+
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        employeeService.CheckAttendance();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice!");
+                        break;
+                }
+
+            } while (choice != 0);
+
+        }
+    }
+
+}
---
+++ oops-csharp-practice/scenario-based/EmployeeWage/EmployeeUtilityImpl.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWage
+{
+    internal class EmployeeUtilityImpl : IEmployee
+    {
+
+        private Random random = new Random();
+
+
+
+        // UC1 - Check Employee is Present or Absent
+        public void CheckAttendance()
+        {
+            int attendance = random.Next(0, 2);
+
+            if (attendance == 1)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+
+    }
+}
