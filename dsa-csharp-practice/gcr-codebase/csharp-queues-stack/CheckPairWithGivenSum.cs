628448074e1fc09a9d6ce49c2dea4d96582b5f51
HaR-S-H <h8551283@gmail.com>
2026-01-09 15:25:06 +0530

    [Harsh Gupta] add CheckPairWithGivenSum

---
+++ dsa-csharp-practice/gcr-codebase/csharp-queues-stack/CheckPairWithGivenSum.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+class PairWithSum
+{
+    public static bool HasPair(int[] arr, int target)
+    {
+        HashSet<int> set = new HashSet<int>();
+
+        foreach (int num in arr)
+        {
+            if (set.Contains(target - num))
+                return true;
+
+            set.Add(num);
+        }
+        return false;
+    }
+}
