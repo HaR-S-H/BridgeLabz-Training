ec1b7adab4072ffa3942426fdf2b136af1d82fa3
HaR-S-H <h8551283@gmail.com>
2026-01-09 15:19:27 +0530

    [Harsh Gupta] add QueueUsingTwoStack

---
+++ dsa-csharp-practice/gcr-codebase/csharp-queues-stack/QueueUsingTwoStack.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+class QueueUsingStacks
+{
+    Stack<int> s1 = new Stack<int>();
+    Stack<int> s2 = new Stack<int>();
+
+    public void Enqueue(int x)
+    {
+        s1.Push(x);
+    }
+
+    public int Dequeue()
+    {
+        if (s2.Count == 0)
+        {
+            while (s1.Count > 0)
+                s2.Push(s1.Pop());
+        }
+
+        return s2.Pop();
+    }
+}
