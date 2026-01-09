86943ff398b82707fb3b1963ffe94e05d173b1e1
HaR-S-H <h8551283@gmail.com>
2026-01-09 15:24:18 +0530

    [Harsh Gupta] add FindSubarrayWithZeroSum

---
+++ dsa-csharp-practice/gcr-codebase/csharp-queues-stack/FindSubarrayWithZeroSum.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+class ZeroSumSubarrays
+{
+    public static void FindSubarrays(int[] arr)
+    {
+        Dictionary<int, List<int>> map = new Dictionary<int, List<int>>();
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+
+            if (sum == 0)
+                Console.WriteLine("Subarray: 0 to " + i);
+
+            if (map.ContainsKey(sum))
+            {
+                foreach (int start in map[sum])
+                    Console.WriteLine($"Subarray: {start + 1} to {i}");
+            }
+
+            if (!map.ContainsKey(sum))
+                map[sum] = new List<int>();
+
+            map[sum].Add(i);
+        }
+    }
+}
