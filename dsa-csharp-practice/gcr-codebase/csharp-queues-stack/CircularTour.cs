91a21b267b731178c19f71a807628099a44ed892
HaR-S-H <h8551283@gmail.com>
2026-01-09 15:23:14 +0530

    [Harsh Gupta] add CircularTour

---
+++ dsa-csharp-practice/gcr-codebase/csharp-queues-stack/CircularTour.cs
@@ -0,0 +1,31 @@
+class PetrolPump
+{
+    public int Petrol, Distance;
+    public PetrolPump(int p, int d)
+    {
+        Petrol = p;
+        Distance = d;
+    }
+}
+
+class CircularTour
+{
+    public static int FindStart(PetrolPump[] pumps)
+    {
+        int start = 0, surplus = 0, deficit = 0;
+
+        for (int i = 0; i < pumps.Length; i++)
+        {
+            surplus += pumps[i].Petrol - pumps[i].Distance;
+
+            if (surplus < 0)
+            {
+                start = i + 1;
+                deficit += surplus;
+                surplus = 0;
+            }
+        }
+
+        return (surplus + deficit >= 0) ? start : -1;
+    }
+}
