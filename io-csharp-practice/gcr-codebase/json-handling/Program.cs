dbf308ffd2c68eb01372e8595c254e40fee33e8e
HaR-S-H <h8551283@gmail.com>
2026-02-02 23:20:11 +0530

    [Harsh Gupta] add JsonHandling

---
+++ io-csharp-practice/gcr-codebase/json-handling/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+class Program
+{
+    static void Main()
+    {
+        var student = new
+        {
+            name = "harsh",
+            age = 22,
+            subjects = new[] { "math", "Science", "English" }
+        };
+        string json =  JsonConvert.SerializeObject(student, Formatting.Indented);
+        Console.WriteLine(json);
+    }
+}
